b62db58ed2d2824e470dbdfb24a5c9e34a3b815a
clj <474691391@qq.com>
2017-12-11 16:08:24 +0800

    add genearate_xml

--- question_generation_1208/config.py
+++ question_generation_1208/config.py
@@ -6,8 +6,9 @@ Authors: zhangruqing(@software.ict.ac.cn)
 Date:    2017/12/02 02:28:12
 File:    config.py
 """
-news_file = 'content_wy.txt'
-decode_file = 'decode_wy.txt'
-model_dir = './model12'
-batch_size = 10
+news_file   = 'data/raw_data/content_wy.txt'
+decode_file = 'data/decode_data/decode_wy.txt'
+model_dir   = 'question_generation_1208/model12'
+vocab_dir   = 'question_generation_1208/vocab.txt'
+batch_size  = 10
 # vim: set expandtab ts=4 sw=4 sts=4 tw=100:
